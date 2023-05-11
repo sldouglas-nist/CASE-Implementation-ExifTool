e8dd93a0b18b94196f20bf4f8b4ecf3f026325b0
Alex Nelson <alexander.nelson@nist.gov>
2023-05-11 08:22:27 -0400

    Bump version
    Signed-off-by: Alex Nelson <alexander.nelson@nist.gov>

--- case_exiftool/__init__.py
+++ case_exiftool/__init__.py
@@ -15,7 +15,7 @@
 This tool parses the RDF output of ExifTool, mapping it into UCO properties and relationships-of-assumption.  An analyst should later annotate the output with their beliefs on its verity.
 """
 
-__version__ = "0.6.0"
+__version__ = "0.7.0"
 
 import argparse
 import contextlib
